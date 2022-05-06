7febcd3d365ea294cf3ee2063fd32a118a60a0be
Bereket Urgessa <bereketadugna15@gmail.com>
2022-05-06 15:44:37 -0400

    add display repository fuctionality

--- js/main.js
+++ js/main.js
@@ -35,4 +35,37 @@ renderData = (user, repos) => {
   document.getElementById("bio").innerText = user.bio;
 
   console.log(repos);
+  //render user repositories - from fetched data
+  repos.forEach((repo) => {
+    userRepos = document.getElementById("user_repos");
+
+    repoName = document.createElement("a");
+    repoName.innerText = repo.name;
+    repoName.href = repo.html_url;
+    userRepos.append(repoName);
+
+    description = document.createElement("p");
+    description.innerText = repo.description;
+    userRepos.append(description);
+
+    repoDetails = document.createElement("div");
+    repoDetails.classList.add("repo_details");
+    userRepos.append(repoDetails);
+
+    language = document.createElement("li");
+    language.innerText = repo.language;
+    repoDetails.append(language);
+
+    stars = document.createElement("li");
+    stars.innerText = repo.stargazers_count;
+    repoDetails.append(stars);
+
+    forks = document.createElement("li");
+    forks.innerText = repo.forks;
+    repoDetails.append(forks);
+
+    updatedAt = document.createElement("li");
+    updatedAt.innerText = repo.updated_at;
+    repoDetails.append(updatedAt);
+  });
 };
