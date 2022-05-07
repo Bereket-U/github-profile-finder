0a32e4da90c3eea816d66dbd4a3f74e2ca1174ca
Bereket Urgessa <bereketadugna15@gmail.com>
2022-05-06 20:33:47 -0400

    update user infrmatio render- create elements in js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const form = document.getElementById("form");
+const userInfo = document.getElementById("user_info");
+const userRepos = document.getElementById("user_repos");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -30,47 +32,60 @@ form.addEventListener("submit", function (e) {
 
 renderData = (user, repos) => {
   //render user information - from fetched data
-  document.getElementById("logo").src = user.avatar_url;
-  document.getElementById("full_name").innerText = user.name;
-  document.getElementById("username").innerText = user.login;
-  document.getElementById("bio").innerText = user.bio;
+  const logo = document.createElement("img");
+  logo.src = user.avatar_url;
+  logo.classList.add("logo");
+  userInfo.append(logo);
+
+  const fullName = document.createElement("h1");
+  fullName.textContent = user.name;
+  userInfo.append(fullName);
+
+  const username = document.createElement("p");
+  username.textContent = user.login;
+  userInfo.append(username);
+
+  const bio = document.createElement("p");
+  bio.textContent = user.bio;
+  userInfo.append(bio);
 
   console.log(repos);
   //render user repositories - from fetched data
   repos.forEach((repo) => {
-    userRepos = document.getElementById("user_repos");
-    // Create div tag to wrap single a repository
-    singleRepo = document.createElement("div");
+    // Create div tag to wrap a single repository
+    const singleRepo = document.createElement("div");
     singleRepo.classList.add("single_repo");
     userRepos.append(singleRepo);
 
-    repoName = document.createElement("a");
+    // Create repository-name and append to singleRepo
+    const repoName = document.createElement("a");
     repoName.innerText = repo.name;
     repoName.href = repo.html_url;
     singleRepo.append(repoName);
 
-    description = document.createElement("p");
+    const description = document.createElement("p");
     description.innerText = repo.description;
     singleRepo.append(description);
 
     //Create div tag to wrap repository details
-    repoDetails = document.createElement("div");
+    const repoDetails = document.createElement("div");
     repoDetails.classList.add("repo_details");
     singleRepo.append(repoDetails);
 
-    language = document.createElement("li");
+    // Create repository-details and append to repoDetails
+    const language = document.createElement("li");
     language.innerText = repo.language;
     repoDetails.append(language);
 
-    stars = document.createElement("li");
+    const stars = document.createElement("li");
     stars.innerText = repo.stargazers_count;
     repoDetails.append(stars);
 
-    forks = document.createElement("li");
+    const forks = document.createElement("li");
     forks.innerText = repo.forks;
     repoDetails.append(forks);
 
-    updatedAt = document.createElement("li");
+    const updatedAt = document.createElement("li");
     updatedAt.innerText = repo.updated_at;
     repoDetails.append(updatedAt);
   });
